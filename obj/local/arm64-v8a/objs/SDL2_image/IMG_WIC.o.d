aa98af9a21fa29d363df8c439dce5620981ec24f
Alexis <120563837+alexisart@users.noreply.github.com>
2023-04-19 00:10:56 -0400

    Updated Android.mk

--- obj/local/arm64-v8a/objs/SDL2_image/IMG_WIC.o.d
+++
@@ -1,2 +0,0 @@
-./android/app/jni/SDL_image/obj/local/arm64-v8a/objs/SDL2_image/IMG_WIC.o: \
-  android/app/jni/SDL_image/IMG_WIC.c
--- obj/local/armeabi-v7a/objs/SDL2_image/IMG_WIC.o.d
+++
@@ -1,2 +0,0 @@
-./android/app/jni/SDL_image/obj/local/armeabi-v7a/objs/SDL2_image/IMG_WIC.o: \
-  android/app/jni/SDL_image/IMG_WIC.c
